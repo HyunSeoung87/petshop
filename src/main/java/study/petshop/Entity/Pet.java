13ae3df19e60a3863c970fac81bf0d992ae346e1
hyun <hyun87@mz.co.kr>
2022-09-14 12:56:22 +0900

    docs: Add API list and TODO

--- src/main/java/study/petshop/Entity/Pet.java
+++ src/main/java/study/petshop/Entity/Pet.java
@@ -13,6 +13,8 @@ public class Pet {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // IDENTITY : 기본키 생성을 DB가 함
+    // SEQUENCE : 유일한 값을 순서대로 매긴다.
     private Long pet_id;
 
     private String pet_name;
@@ -20,6 +22,8 @@ public class Pet {
     private Integer pet_old;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    // Lazy : 지연 로딩 -> 연관된 엔티티를 프록시 객체로 가져와서 DB의 부담을 줄여준다. 같이 가져와야하는 상황이라면 SELECT가 2번 된다.
+    // EAGER : 즉시 로딩 -> 연관된 엔티티를 바로 가져온다.
     @JoinColumn(name = "member_id")
     private Member member;
 }
