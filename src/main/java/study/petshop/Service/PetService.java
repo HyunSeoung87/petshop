35024e890072f54d445ab395ead045c5a636619a
hyun <hyun87@mz.co.kr>
2022-09-16 13:22:52 +0900

    feat: Add getPet

--- src/main/java/study/petshop/Service/PetService.java
+++ src/main/java/study/petshop/Service/PetService.java
@@ -7,6 +7,7 @@ import study.petshop.Repository.PetRepository;
 import study.petshop.dto.PetDto;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -23,5 +24,22 @@ public class PetService {
                     .pet_name(p.getPet_name())
                     .member_id(p.getMember())
                     .build()).collect(Collectors.toList());
+    }
+
+    public Long create(PetDto petDto) {
+        Pet pet = new Pet(petDto.getPet_id(), petDto.getPet_name(), petDto.getPet_old(), petDto.getMember_id());
+        petRepository.save(pet);
+        PetDto petDto1 = new PetDto(pet.getPet_id(), pet.getPet_name(), pet.getPet_old(), pet.getMember());
+
+        return petDto1.getPet_id();
+    }
+
+    public PetDto getPet(Long id) {
+        Optional<Pet> pet = petRepository.findById(id);
+        if (pet.isPresent()) {
+            Pet pet1 = pet.get();
+            return new PetDto(pet1.getPet_id(), pet1.getPet_name(), pet1.getPet_old(), pet1.getMember());
+        }
+        return null;
     }
 }
